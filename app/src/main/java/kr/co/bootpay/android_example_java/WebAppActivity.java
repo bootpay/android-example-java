0fcc99f7d2fabef5797539cd82c7ecdc246b0771
ehowlsla <rupy1014@gmail.com>
2022-06-10 09:31:51 +0900

    first commit

---
+++ app/src/main/java/kr/co/bootpay/android_example_java/WebAppActivity.java
@@ -0,0 +1,23 @@
+package kr.co.bootpay.android_example_java;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import kr.co.bootpay.android.webview.BootpayWebView;
+
+public class WebAppActivity extends AppCompatActivity {
+//    BootpayWebView bootpayWebView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webapp);
+
+        BootpayWebView webView = findViewById(R.id.webview);
+
+        //link your domain
+        webView.loadUrl("https://d-cdn.bootapi.com/test/payment/");
+    }
+}
